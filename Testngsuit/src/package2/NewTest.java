bebea755aa3b3baaf913b78eab52bd481ad565ce
yogesh.kkr19 <yogesh.kkr19@WIN-R1KUFKDM66H.localdomain>
2017-01-27 16:31:57 +0530

    first changes done

---
+++ Testngsuit/src/package2/NewTest.java
@@ -0,0 +1,21 @@
+package package2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  WebDriver driver=new HtmlUnitDriver();
+	  driver.get("https://www.google.com");
+	 
+	  System.out.println("this is package2 test");
+	  
+	  System.out.println( driver.getTitle());
+	  
+	  System.out.println( "changeses1");
+  }
+}
