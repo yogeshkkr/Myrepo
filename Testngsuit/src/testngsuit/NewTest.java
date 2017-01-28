342abc559bca3ff51b9c5a307707be69c5fb609d
yogeshkkr <preethi.kumari2020@gmail.com>
2017-01-28 20:11:14 +0530

    testngsuit checked in

---
+++ Testngsuit/src/testngsuit/NewTest.java
@@ -0,0 +1,13 @@
+package testngsuit;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("this is running fron package name testngsuit");
+	  
+	  System.out.println("Changes1");
+  }
+}
